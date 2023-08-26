7f43373d2d01d571a2fda739ed8b445c4e4bd565
ykwksxzq <132530452+ykwksxzq@users.noreply.github.com>
2023-08-26 02:23:07 +0000

    ゲストログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -21,8 +21,12 @@ devise_for :users,skip: [:passwords], controllers: {
 
 scope module: :public do
   root to: "homes#top"
+  
 end
 
+devise_scope :user do
+  post 'users/guest_sign_in', to: 'public/sessions#guest_sign_in'
+end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
