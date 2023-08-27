193e2e02368db3499bcc936d5fb5252020f8115d
ykwksxzq <132530452+ykwksxzq@users.noreply.github.com>
2023-08-27 04:44:57 +0000

    ジャ編集

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -11,6 +11,17 @@ class Admin::GenresController < ApplicationController
     redirect_to admin_genres_path
   end
 
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+
+  def update
+    @genre = Genre.find(params[:id])
+    @genre.update(genre_params)
+    redirect_to admin_genres_path
+  end
+
   private
 
   def genre_params
