08bb78b834856a69ff2823ce0982905cfb135471
ykwksxzq <132530452+ykwksxzq@users.noreply.github.com>
2023-09-30 02:56:04 +0000

    admin　一覧表示　レイアウト

--- app/controllers/admin/item_lists_controller.rb
+++ app/controllers/admin/item_lists_controller.rb
@@ -1,9 +1,9 @@
 class Admin::ItemListsController < ApplicationController
-  
+
   def index
-    @item_lists = ItemList.all
+    @item_lists = ItemList.all.page(params[:page]).per(12)
   end
-  
+
   def show
     @item_list = ItemList.find(params[:id])
   end
