31feffaf23bea33ece521b8d9368a02641346c97
ykwksxzq <132530452+ykwksxzq@users.noreply.github.com>
2023-10-31 10:51:21 +0000

    デプロイ環境修正

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -2,19 +2,19 @@ class Public::FavoritesController < ApplicationController
  before_action :authenticate_user!
  
  def create
-    post_craft = PostCraft.find(params[:post_craft_id])
-    favorite = current_user.favorites.new(post_craft_id: post_craft.id)
-    favorite.save
-    flash[:notice] = "いいねしました"
-    redirect_to post_craft_path(post_craft)
+  post_craft = PostCraft.find(params[:post_craft_id])
+  favorite = current_user.favorites.new(post_craft_id: post_craft.id)
+  favorite.save
+  flash[:notice] = "いいねしました"
+  redirect_to post_craft_path(post_craft)
  end
 
  def destroy
-    post_craft = PostCraft.find(params[:post_craft_id])
-    favorite = current_user.favorites.find_by(post_craft_id: post_craft.id)
-    favorite.destroy
-    flash[:notice] = "いいねを外しました"
-    redirect_to post_craft_path(post_craft)
+  post_craft = PostCraft.find(params[:post_craft_id])
+  favorite = current_user.favorites.find_by(post_craft_id: post_craft.id)
+  favorite.destroy
+  flash[:notice] = "いいねを外しました"
+  redirect_to post_craft_path(post_craft)
  end
 
 
